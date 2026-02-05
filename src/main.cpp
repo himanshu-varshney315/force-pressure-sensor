2d5dc4a9936bf4e06603f78706f017a3b2377a36
himanshu-varshney315 <himanshu.gupta_cs.aiml24@gla.acin>
2026-02-05 09:11:40 +0530

    Initial commit

---
+++ src/main.cpp
@@ -0,0 +1,14 @@
+#include <Arduino.h>
+int value=0;
+void setup() {
+// write your initialization code here
+    Serial.begin(9600);
+    pinMode(A0, OUTPUT);
+}
+
+void loop() {
+// write your code here
+    value=analogRead(A0);
+    Serial.println(value);
+    delay(500);
+}
